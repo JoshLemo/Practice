768267730ea8f7a8a45454f9983b10cd6a5bae6f
joshl <joshl@DESKTOP-5NOH99V.T-mobile.com>
2018-01-30 15:39:22 -0500

    test

---
+++ JLemonakisIntegrationProject/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// Josh Lemonakis
+// My project
+
+public class Main {
+	
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    	
+    	System.out.println("What is your name?");
+    	String userName = scan.nextLine();
+    	System.out.println(userName.sub);
+    	
+    	System.out.println("Enter x1"); // prompt
+    	int x1 = scan.nextInt();// get int input
+    	
+    	System.out.println("Enter rate");// prompt
+    	double rate = scan.nextDouble();
+    	
+    }
+}
